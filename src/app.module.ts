a2c61158a57aa6c9b973fe7f99c6ff0de1205257
LucasOliveiraSantana <lucasoliveirasantana.dev@gmail.com>
2022-12-16 14:52:59 -0300

    :tada: First Class & Project structure

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { DatabaseModule } from './database/database.module';
+import { StudentsModule } from './modules/students/students.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    DatabaseModule,
+    StudentsModule,
+  ],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
